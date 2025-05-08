ef949c89b44e8bf318d584d1b968d7fa7558da7e
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-05-08 12:38:08 -0300

    12:37 08/05/2025

--- JAVASCRIPT/rotas.js
+++ JAVASCRIPT/rotas.js
@@ -1,7 +1,10 @@
 function changetab(tab){
 
     document.getElementById('todastab').style.display = tab === 'todas' ? 'block' : 'none';
+    document.getElementById('todascontent').style.display = tab === 'todas' ? 'block' : 'none';
     document.getElementById('atuaisstab').style.display = tab === 'atuais' ? 'block' : 'none';
+    document.getElementById('atuaiscontent').style.display = tab === 'atuais' ? 'block' : 'none';
     document.getElementById('alertasstab').style.display = tab === 'alertas' ? 'block' : 'none';
+    document.getElementById('alertascnotent').style.display = tab === 'alertas' ? 'block' : 'none';
 
 }
