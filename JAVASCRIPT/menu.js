b2fefe8d44635f4426ead2d5f79c807ed00c2744
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-06-04 12:47:35 -0300

    04/06/2025 12:47

--- JAVASCRIPT/menu.js
+++ JAVASCRIPT/menu.js
@@ -1,3 +1,21 @@
+const icon = document.getElementById("icon");
+const menuButton = document.getElementById("menuButton");
+
+let closed = true;
+
+menuButton.addEventListener("click", function() {
+  
+  if (closed){
+    icon.src = "../IMAGENS/x_icon_172101.png";
+  } else{
+    icon.src = "../IMAGENS/Hamburger_icon.svg.png"
+  }
+  closed = !closed;
+
+});
+
+
 function openav(){
     document.getElementById('navbar').style.display = document.getElementById('navbar').style.display === 'none' ? 'block' : 'none' ;
 }
+
