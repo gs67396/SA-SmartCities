2f234b11ff2ae7db051be5d4fb65c22cd8446ab2
Guilhermeramosalvim <guilherme_alvim@estudante.sesisenai.org.br>
2025-06-09 11:28:09 -0300

    Update Progresso.js

--- JAVASCRIPT/Progresso.js
+++ JAVASCRIPT/Progresso.js
@@ -1,19 +1,23 @@
-  const progresso = document.querySelector('.progresso');
+const energyBar = document.getElementById('energyBar');
+let currentEnergy = 0;
+let isCharging = true;
 
-    // Função para simular a carga de energia
-    function carregar(intervalo) {
-        let largura = 0;
-        const intervalo = setInterval(frame, 20); // Velocidade da animação
-
-        function frame() {
-            if (largura >= 100) {
-                clearInterval(intervalo);
-            } else {
-                largura++;
-                progresso.style.width = largura + '%';
-            }
-        }
+function updateEnergyBar() {
+    if (isCharging) {
+        currentEnergy += 2; // Aumenta a energia
+    } else {
+        currentEnergy -= 2; // Diminui a energia
     }
 
-    // Iniciar a animação
-    carregar();
+    if (currentEnergy > 100) {
+        currentEnergy = 100; // Limite superior
+        isCharging = false;
+    } else if (currentEnergy < 0) {
+        currentEnergy = 0; // Limite inferior
+        isCharging = true;
+    }
+
+    energyBar.style.width = currentEnergy + '%';
+}
+
+setInterval(updateEnergyBar, 1000); // Atualiza a barra a cada segundo
