acd916c34beea996a210aa3527ed75d3bc3d70f4
Guilhermeramosalvim <guilherme_alvim@estudante.sesisenai.org.br>
2025-09-22 12:06:19 -0300

    Update novaconta.php: Correcao nome da pagina

--- PHP/novaconta.php
+++ PHP/novaconta.php
@@ -63,7 +63,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/loginstyle.css">
-    <title>Document</title>
+    <title>Cadastro</title>
 </head>
 
 <body>
