0babebebb289e8f66488ec6c306b144b439981cd
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-10-17 11:05:19 -0300

    reorganizando o banco de dados

--- SQL/Sensores.sql
+++
@@ -1,8 +0,0 @@
-
-
-INSERT INTO sensores (id_sensor, nome_sensor, tipo_sensor, localizacao_trem) VALUES
-(1, 'Sensor de Temperatura', 'Temperatura', 'Trem 001'),
-(2, 'Sensor de Umidade', 'Umidade', 'Trem 002'),
-(3, 'Sensor de Pressão', 'Pressão', 'Trem 003'),
-(4, 'Sensor de Proximidade', 'Proximidade', 'Trem 004');
-
--- SQL/rotas.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO rota (nome_rota,origem_rota,destino_rota,distancia_rota) VALUES
-('Ida_porto', 'Joinville', 'Jaragua do Sul',17);
-INSERT INTO rota (nome_rota,origem_rota,destino_rota,distancia_rota) VALUES
-('Volta_porta', 'Joinville', 'Estação Rio Vermelho',29);
-
-INSERT INTO rotas_trem (pk_trem, pk_rota,data_hora_rota) VALUES
-(1,1,'2025-11-11 15:00:00');
-
