ccf1f6d899fdb78f182372e240f5dc9b32c11154
guilhermeramosalvim <guilherme_alvim@estudante.sesisenai.org.br>
2025-10-22 12:29:39 -0300

    Animacao da barra menu

--- PHP/CODIGO/menu.php
+++ PHP/CODIGO/menu.php
@@ -1,7 +1,9 @@
 
     <div class='navbar' id='navbar' style='display: none;'>
+        <div class='fechar'>
         <div class='home'><button id='menuButtonClose' onclick='openav()' style="display: none"><img id='icon'
-                src='../../IMAGENS/x_icon_172101.png'></button></div>
+         src='../../IMAGENS/x_icon_172101.png'></button></div> 
+        </div>
     <script src='../../JAVASCRIPT/menu.js'></script>
         <a href='../PAGINAS/Inicio.php'><div><img src='../../IMAGENS/SmartTrain.png' alt=''></div></a>
         <a href='../PAGINAS/Inicio.php'>Inicio</a>
@@ -12,5 +14,3 @@
         <a href='../PAGINAS/Gerenciamento_sensores.php'>Sensores</a>
         <a href='../PAGINAS/Manual_usuario.php'>Ajuda</a>
     </div>
-    
-
