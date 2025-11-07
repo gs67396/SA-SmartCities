de2c333cc09c16999c9b010ffb0ab125a818dc8c
guilhermeramosalvim <guilherme_alvim.estudante.sesisenai.org.br>
2025-11-07 10:12:33 -0300

    Pagina sobre nós

---
+++ PHP/PAGINAS/Sobre_nós.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CSS/traininfostyle.css">
+    <link rel="stylesheet" href="../../CSS/menu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+    <title>Manual do usuario</title>
+</head>
+
+<body>
+    <?php include("../CODIGO/menu.php"); ?>
+    <div class='home'><button id='menuButtonOpen' onclick='openav()'><img id='icon'
+                src='../../IMAGENS/Hamburger_icon.svg.png'></button>
+    </div>
+    <script src='../../JAVASCRIPT/menu.js'>
+    </script>
+    <div class="infoLogo">Sobre nós</div>
+    <hr>
+
+    <br>
+    <h2>Introdução</h2>
+    <br>
+    <div class="box free">
+        <p style="
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">Bem-vindo ao SamrtTrain! Aqui contaremos sobre nós, saber nossa historia e objetio para esse sistema.</p>
+
+    </div>
+
+    <div class="bigbox">
+        <details class="selecao">
+            <summary>1. Nossa Missão</summary>
+            <p></p>
+            <p></p>
+        </details>
+    </div>
+
+     <div class="bigbox">
+        <details class="selecao">
+            <summary>2. Nossa História</summary>
+            <p></p>
+            <p></p>
+        </details>
+    </div>
+
+    <div class="bigbox">
+        <details class="selecao">
+            <summary>3. O Que Oferecemos:</summary>
+            <details class="selecao">
+                <p>Smart Train, você encontrará:</p>
+                <summary>Guias</summary>
+                <p></p>
+            </details>
+            <details class="selecao">
+                <summary>Segurança</summary>
+                <p></p>
+            </details>
+        </details>
+    </div>
+
+    <div class="bigbox">
+        <details class="selecao">
+            <summary>4. Produtos Selecionados:</summary>
+            <p>Uma curadoria de locomotivas, vagões, trilhos, prédios e acessórios das melhores marcas do mercado (se aplicável, para sites de e-commerce).</p>
+            <p>Notícias e Eventos: Fique por dentro das novidades do mundo do ferromodelismo e dos eventos presenciais e online.</p>
+        </details>
+    </div>
+
+    <div class="bigbox">
+        <details class="selecao">
+            <summary>5.Conecte-se Conosco</summary>
+            <p>Convidamos você a explorar nosso site, tirar suas dúvidas e, o mais importante, compartilhar sua própria jornada sobre os trilhos. Porque, no fim das contas, não é apenas sobre trens. É sobre as histórias que criamos, as memórias que construímos e as conexões que fazemos ao longo do caminho.</p>
+        </details>
+    </div>
+
+    <div class="bigbox">
+        <details class="selecao">
+            <summary>6 Contatos</summary>
+            <p>Telefone: 47 98881 9133</p>
+            <p>Email: Suporte_trem@smartraimail.com</p>
+        </details>
+    </div>
+
+</body>
+
+</html>
